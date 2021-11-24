f0a335b41b3d0f83fc2fdbad690aa3eee60d1460
Rutvik-prajapati <rutvikprajapati08@gmail.com>
2021-11-24 16:17:37 +0530

    REFS : #1947 and fixes #1947 Day 6 c#

---
+++ C#/Rutvik Prajapati/Day6/Assignment/Assignment1Day6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTask.Day6.Assignment
+{
+    //Compute area of rectangle using func delegate
+
+    public delegate void calculate(int a, int b);
+    class Assignment1Day6
+    {
+        public static void RectangleArea(int l,int b)
+        {
+            var result = l * b;
+            Console.WriteLine("Rectangle Area Result : {0}",result);
+        }
+        static void Main(string[] args)
+        {
+            calculate cal = new calculate(Assignment1Day6.RectangleArea);
+            Console.WriteLine("Enter Rectangel length :");
+            var length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Rectangel width :");
+            var width = Convert.ToInt32(Console.ReadLine());
+            cal.Invoke(length, width);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Rutvik Prajapati/Day6/Assignment/Assignment2Day6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyTask.Day6.Assignment
+{
+    //Compute add of two number using lambda expression
+
+    public delegate int Addition(int a, int b);
+    class Assignment2Day6
+    {
+        static void Main(string[] args)
+        {
+            //expression lamda..
+            Addition obj = (a, b) => a + b;
+            Console.WriteLine("Enter First Number :");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number :");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+            var result = obj(num1, num2);
+            Console.WriteLine("Result of two number is : {0}",result);
+            Console.ReadLine();
+
+            //statement lamda..
+            //Addition obj = delegate (int a, int b)
+            //{
+
+            //};
+        }
+    }
+}
